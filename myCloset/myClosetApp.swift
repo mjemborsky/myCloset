49b10e66e918827c375ee487d70a4f54e4cd622d
jonahgloss <jeg4241@uncw.edu>
2023-02-14 11:29:18 -0500

    Initial Commit

---
+++ myCloset/myClosetApp.swift
@@ -0,0 +1,17 @@
+//
+//  myClosetApp.swift
+//  myCloset
+//
+//  Created by Jonah Gloss on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct myClosetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
