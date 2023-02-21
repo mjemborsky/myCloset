10b14d30f032db5e2661143d0366ba547a3a53e5
alexar5035 <alexar5035@gmail.com>
2023-02-21 13:50:00 -0500

    Update outfit.swift
    finished data class except for importing photos

--- myCloset/outfit.swift
+++ myCloset/outfit.swift
@@ -6,7 +6,24 @@
 //
 
 import Foundation
-//Outfit:
+class outfit{
+    var title: String
+    var outfitTag: String
+    var  outfitID: Int
+    
+    init(title: String, tag: String, outfitID: Int){
+        self.title = title
+        self.outfitTag = tag
+        self.outfitID = outfitID
+    }
+}
+
+let SAMPLE_OUTFIT:[outfit] = [
+    outfit(title:"Date", tag:"dress", outfitID:1),
+    outfit(title:"Picnic", tag:"skirt", outfitID:2),
+    outfit(title:"School", tag:"pants", outfitID:3)
+]
+
 //Outfit ID
 //PhotoFile
 //Title
