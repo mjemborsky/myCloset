ad21aa11e8b07df8c3e139adcf8029b18a3babf5
rclounsb <104331742+rclounsb@users.noreply.github.com>
2023-02-21 13:42:24 -0500

    added userstate to gitignore

--- myCloset/ClothingItem.swift
+++ myCloset/ClothingItem.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 class ClothingItem {
-
+    
     var ItemID: Int
     var ItemTag: String
     var ItemPhoto: String
-
-
+    
+    
     /// constructor
     init(newID: Int, newTag: String, newPhoto: String) {
         self.ItemID = newID
