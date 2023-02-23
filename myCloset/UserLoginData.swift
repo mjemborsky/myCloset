e54c11226e9319bfc2a404add38b2d507cca24f2
jonahgloss <jeg4241@uncw.edu>
2023-02-23 10:00:51 -0500

    Added sample data to UserLoginData class

--- myCloset/UserLoginData.swift
+++ myCloset/UserLoginData.swift
@@ -21,3 +21,8 @@ class UserLoginData {
 
     }
 }
+
+let SAMPLE_USERLOGINDATA:[UserLoginData] = [
+    UserLoginData(username: "jonahgloss", password: "myclosetfan21", email: "jonahgloss@gmail.com"),
+    UserLoginData(username: "elicopeland", password: "rockymountain11", email: "elicopeland@gmail.com"),
+    UserLoginData(username: "rielymurray", password: "tinroof365", email: "rielymurray@gmail.com")]
