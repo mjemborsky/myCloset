22078f4746499f93eadaaec51620973582efb1b9
mjemborsky <michaelemborsky@gmail.com>
2023-03-04 00:02:03 -0500

    added UI files and set up the feed interface

---
+++ myCloset/PostCell.swift
@@ -0,0 +1,20 @@
+//
+//  PostCell.swift
+//  myCloset
+//
+//  Created by Michael Emborsky on 3/3/23.
+//
+
+import SwiftUI
+
+struct PostCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostCell_Previews: PreviewProvider {
+    static var previews: some View {
+        PostCell()
+    }
+}
---
+++ myCloset/PostViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PostViewModel.swift
+//  myCloset
+//
+//  Created by Michael Emborsky on 3/3/23.
+//
+
+import Foundation
