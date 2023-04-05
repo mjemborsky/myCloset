6a89ffcc3d237bd38ecd844fc482d8a8dff75aa0
tmcmillon <tam8710@uncw.edu>
2023-04-05 14:54:43 -0400

    adding prfile view

---
+++ myCloset/CollageGridView.swift
@@ -0,0 +1,20 @@
+//
+//  CollageGridView.swift
+//  myCloset
+//
+//  Created by Taylor  on 4/5/23.
+//
+
+import SwiftUI
+
+struct CollageGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CollageGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        CollageGridView()
+    }
+}
