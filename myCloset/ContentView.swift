0acf68bb500dc9220110621a257de38a3e21aa72
alexar5035 <alexar5035@gmail.com>
2023-04-09 21:10:14 -0400

    Revert "attempt to combine tag with photo upload"
    This reverts commit f77753051882d6beb134d32b33d311a002125790.

--- myCloset/ContentView.swift
+++ myCloset/ContentView.swift
@@ -153,7 +153,6 @@ struct ContentView: View {
     }
 }
 
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
--- myCloset/NewClothingItemView.swift
+++ myCloset/NewClothingItemView.swift
@@ -6,108 +6,27 @@
 //
 
 import SwiftUI
-import FirebaseStorage
-import FirebaseFirestore
-
 
 struct NewClothingItemView: View {
     @EnvironmentObject var clothingItemManager: ClothingItemManager
     @State private var newClothingItem = ""
-    @State var isPickerShowing = false
-    @State var selectedImage: UIImage?
-    @State var retrievedImages = [UIImage]()
     
     var body: some View {
         VStack {
-            if selectedImage != nil{
-                Image(uiImage: selectedImage!)
-                    .resizable()
-                    .frame(width: 200, height: 200)
-            }
+            TextField("Clothing Item", text:$newClothingItem)
             
-            Button{
-                
-                // Show the image picker
-                isPickerShowing = true
-                
-            } label:{
-                Text("Select a Photo")
+            Button {
+                clothingItemManager.addClothingItem(newItemPhoto: newClothingItem)
+            } label : {
+                Text("Save")
             }
-            
-            // Upload button
-            if selectedImage != nil{
-                Button {
-                    // Upload the image
-                    uploadPhoto()
-                    
-                } label: {
-                    Text("Upload photo")
-                }
-                Divider()
-                TextField("ItemTag", text: $newClothingItem)
-            }
-            
         }
-        .sheet(isPresented: $isPickerShowing, onDismiss: nil){
-            // Image picker
-            ImagePicker(selectedImage: $selectedImage, isPickerShowing: $isPickerShowing)
-        }
-        
+        .padding()
     }
+}
 
-    
-    // content view
-    
-    func uploadPhoto(){
-        
-        
-        // Make sure that the selected image property isn't nil
-        guard selectedImage != nil else {
-            return
-        }
-        
-        // Create storage reference
-        let storageRef =  Storage.storage().reference()
-        
-        // Turn our image into data
-        let imageData = selectedImage!.jpegData(compressionQuality: 0.8)
-        
-        guard imageData != nil else {
-            return
-        }
-        
-        // Specify the file path and name
-        let path = "images/\(UUID().uuidString).jpg"
-        let fileRef = storageRef.child(path)
-        
-        // Upload that data
-        let uploadTask = fileRef.putData(imageData!, metadata: nil) { metadata, error in
-            // Check for errors
-            
-            if error == nil && metadata != nil {
-                // Save a reference to the file in Firestore DB
-                let db = Firestore.firestore()
-                db.collection("images").document().setData(["ItemTag" : $newClothingItem, "url" : path]){ error in
-                    if let error = error {
-                        print(error.localizedDescription)
-                    }
-                    // If there were no errors, display the new image
-//                    if error == nil {
-//
-//                        DispatchQueue.main.async {
-//                            // add the uploaded image to the list of images for display
-//                            self.retrievedImages.append(self.selectedImage!)
-//                        }
-                        
-                    } // end error(db.collection
-                } // end db.collection
-            } // end error(uploadTask)
-        } // end uploadTask
-    } // end UploadPhoto
-    
-    struct NewClothingItemView_Previews: PreviewProvider {
-        static var previews: some View {
-            NewClothingItemView()
-        }
+struct NewClothingItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewClothingItemView()
     }
-//end
+}
