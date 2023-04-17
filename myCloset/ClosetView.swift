cc7f08cc59ae9fa2e966c014012dd6e3ec56e783
tmcmillon <tam8710@uncw.edu>
2023-04-17 16:49:58 -0400

    make closet items selectable and append the items to a list

--- myCloset/ClosetView.swift
+++ myCloset/ClosetView.swift
@@ -13,6 +13,7 @@ struct ClosetView: View {
     @EnvironmentObject var clothingItemManager: ClothingItemManager
     
     @State private var showPopup = false
+    @State private var isEditing = false
     
     // Variable for if sidemenu is showing or not
     @State private var toggleMenu: Bool = false
@@ -24,25 +25,32 @@ struct ClosetView: View {
     // Variable to hide feedview
     @State private var isHidden: Bool = false
     
+    @State private var selectedItems: [ClothingItem] = []
+    
+    
     var body: some View {
         ZStack {
             NavigationView {
-                List(clothingItemManager.clothingItems, id: \.ItemTag) { clothingItem in (Text(clothingItem.ImageURL))
-                    
+                List(clothingItemManager.clothingItems, id: \.ItemTag) { clothingItem in
+                    if isEditing {
+                        CheckboxRow(clothingItem: clothingItem, selectedItems: $selectedItems)
+                    } else {
+                        (Text(clothingItem.ImageURL))
+                    }
                 }
+            
                 
                 .navigationTitle("Closet")
-                
-                .navigationBarItems(trailing: Button(action: {
-                    
-                    showPopup.toggle()
-                    
+                .navigationBarItems(leading: Button(action: {
+                        isEditing.toggle() // <-- Toggle editing mode
                 }, label: {
-                    
+                    Text(isEditing ? "Done" : "Select")
+                        .padding()
+                }), trailing: Button(action: {
+                    showPopup.toggle()
+                }, label: {
                     Image(systemName: "plus")
-                    
                 }))
-                
                 .sheet(isPresented: $showPopup) {
                     
                     ContentView()
@@ -62,6 +70,11 @@ struct ClosetView: View {
                     returnToView()
                 }
         }
+        .onChange(of: isEditing) { newValue in
+            if !newValue {
+                print(selectedItems) // <-- Print the selected items
+            }
+        }
         
     }
     func returnToView() {
@@ -73,19 +86,41 @@ struct ClosetView: View {
     }
 }
 
+struct CheckboxRow: View {
+    
+    @State var isChecked: Bool = false
+    let clothingItem: ClothingItem
+    @Binding var selectedItems: [ClothingItem]
+    
+    var body: some View {
+        HStack {
+            Image(systemName: isChecked ? "checkmark.circle" : "circle")
+                .onTapGesture {
+                    isChecked.toggle()
+                    if isChecked {
+                        selectedItems.append(clothingItem)
+                    } else {
+                        selectedItems.removeAll(where: { $0.ImageURL == clothingItem.ImageURL})
+                    }
+                }
+            Text(clothingItem.ImageURL)
+        }
+    }
+}
 
+struct ContentofClosetView: View {
+    @StateObject var clothingItemManager = ClothingItemManager()
 
+    var body: some View {
+        ClosetView().environmentObject(clothingItemManager)
+    }
+}
 
-
-
-
-struct ClosetView_Previews: PreviewProvider {
+struct ContentofCloset_Previews: PreviewProvider {
 
     static var previews: some View {
 
-        ClosetView()
-
-            .environmentObject(ClothingItemManager())
+        ContentofClosetView()
 
     }
 
--- myCloset/ClothingItemManager.swift
+++ myCloset/ClothingItemManager.swift
@@ -14,6 +14,7 @@ import FirebaseStorage
 class ClothingItemManager: ObservableObject {
 
     @Published var clothingItems: [ClothingItem] = []
+ 
 
     
 
