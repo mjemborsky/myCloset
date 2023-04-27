7b9e578174c66008e99ead55e4708a711f270c2b
jonahgloss <jeg4241@uncw.edu>
2023-04-26 20:24:44 -0400

    Screenshot crop functionality

--- myCloset/saveOutfitView.swift
+++ myCloset/saveOutfitView.swift
@@ -18,12 +18,21 @@ struct saveOutfitView: View {
     @State private var showFeedView = false
     let db = Firestore.firestore()
     let outfitid = UUID().uuidString
+    @State private var selectedRect: CGRect?
     
     var body: some View {
         NavigationView{
             VStack{
                 Spacer()
                 SelectedItemsView()
+                    .background(
+                        GeometryReader { proxy in
+                            Color.clear
+                                .onAppear {
+                                    selectedRect = CGRect(x: 0, y: 100, width: 400, height: 500)
+                                }
+                        }
+                    )
                     .navigationTitle("Save Post")
                     .toolbar {
                         ToolbarItem(placement: .navigationBarTrailing) {
@@ -53,8 +62,10 @@ struct saveOutfitView: View {
                 
                 Button(action: {
                     createPost(postCreator: "username", postDescription: "My favorite outfit", postTags: ["trendy", "purple", "sweater"], postImage: "", linkedOutfit: outfitid)
-                    let image = takeScreenshot()
-                    uploadScreenshot(image)
+                    if let selectedRect = selectedRect {
+                        let image = takeScreenshot(rect: selectedRect)
+                        uploadScreenshot(image)
+                    }
                     self.showFeedView = true
                 }, label: {
                     Text("Post").padding()
@@ -66,10 +77,10 @@ struct saveOutfitView: View {
         }
     }
     
-    func takeScreenshot() -> UIImage {
+    func takeScreenshot(rect: CGRect) -> UIImage {
         let format = UIGraphicsImageRendererFormat()
         format.opaque = false
-        let renderer = UIGraphicsImageRenderer(size: UIScreen.main.bounds.size, format: format)
+        let renderer = UIGraphicsImageRenderer(bounds: rect, format: format)
         let image = renderer.image { context in
             UIApplication.shared.windows.first?.rootViewController?.view.layer.render(in: context.cgContext)
         }
